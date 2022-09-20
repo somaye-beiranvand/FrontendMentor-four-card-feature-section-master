9f05cd4d105fa583d39476952ff1c55b8ddb4209
unknown <beiranvand.somaye@gmail.com>
2022-09-20 20:49:09 +0430

    fix animation bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const card = $(".sm-card");
-  $.appear(card, {});
-  card.on("appear", function () {
+  //   const card = $(".sm-card");
+  $(".sm-card:in-viewport").animate({ opacity: "1" }, "slow");
+  $.appear(".sm-card", {});
+  $(".sm-card").on("appear", function () {
     $(this).animate({ opacity: "1" }, "slow");
   });
 });
