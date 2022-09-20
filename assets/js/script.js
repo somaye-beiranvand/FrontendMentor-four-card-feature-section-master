fd9cdccba57543a97450c0b4de39df8e8cb79b79
unknown <beiranvand.somaye@gmail.com>
2022-09-20 20:59:13 +0430

    increase time that animation happens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   //   const card = $(".sm-card");
-  $(".sm-card:in-viewport").animate({ opacity: "1" }, "slow");
+  $(".sm-card:in-viewport").animate({ opacity: "1" }, 2000);
   $.appear(".sm-card", {});
   $(".sm-card").on("appear", function () {
     $(this).animate({ opacity: "1" }, "slow");
