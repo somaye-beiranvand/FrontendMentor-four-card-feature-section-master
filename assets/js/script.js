ee336135258780bf6c9212b3fbd747f53d86eace
unknown <beiranvand.somaye@gmail.com>
2022-09-20 18:12:00 +0430

    add animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const card = $(".sm-card");
-//   $.appear(card, {});
-//   card.on("appear", function () {
-//     $(this).animate({ opacity: "1" }, "slow");
-//   });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  const card = $(".sm-card");
+  $.appear(card, {});
+  card.on("appear", function () {
+    $(this).animate({ opacity: "1" }, "slow");
+  });
+});
